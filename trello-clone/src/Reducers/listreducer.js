bb2d70cf9b2464397afcb889dc4096a869ed66f4
Rishab Kumar <k.rishab@coffeebeans.io>
2020-04-20 19:41:10 +0530

    Final trello clone

--- trello-clone/src/Reducers/listreducer.js
+++ trello-clone/src/Reducers/listreducer.js
@@ -31,20 +31,34 @@ const initialState = {
             description:"Reducers for actions"
         },
         {
-            cardId:4,
+            cardId:3,
             belongsToList:1,
-            title:"Actions",
+            title:"Debugging",
             description:""
         },
         {
-            cardId:3,
+            cardId:4,
             belongsToList:2,
-            title:"Debugging",
+            title:"Re ordering logic",
             description:""
-        }
+        },
     
     ]
 } 
+
+function compare(a, b) {
+    // Use toUpperCase() to ignore character casing
+    const list1 = a.belongsToList;
+    const list2 = b.belongsToList;
+  
+    let comparison = 0;
+    if (list1 > list2) {
+      comparison = 1;
+    } else if (list1 < list2) {
+      comparison = -1;
+    }
+    return comparison;
+  }
 
 
 
@@ -73,6 +87,12 @@ const ListsReducer = (state= initialState, action) => {
             let newCardArray = [...state.cardArray];
             newCardArray.push(newCard);
 
+            
+              
+              newCardArray.sort(compare);
+
+              console.log("NEWWW ARAYYYYY",newCardArray);
+
             return { ...state,cardArray:newCardArray};
           
 
@@ -82,29 +102,84 @@ const ListsReducer = (state= initialState, action) => {
                 droppableIndexEnd,
                 droppableIndexStart,
                 draggableId } = action.payload;
+            
             let newCardArrayDrag = [...state.cardArray];
 
             let cardIdcheck = parseInt(draggableId);
-            let droppableIdcheck = parseInt(droppableIdEnd);
+            let droppableIdEndcheck = parseInt(droppableIdEnd);
+            let droppableIdStartcheck = parseInt(droppableIdStart);
 
-            if(droppableIdStart === droppableIdEnd) {
-                let addCard = newCardArrayDrag.splice(droppableIndexStart, 1);
-                newCardArrayDrag.splice(droppableIndexEnd, 0, addCard[0]);
-            }
+            if(droppableIdStartcheck === droppableIdEndcheck) {
 
-            if(droppableIdStart !== droppableIdEnd){
-                
-                for(i in newCardArrayDrag){
-                    if(newCardArrayDrag[i].cardId===cardIdcheck){
-                        newCardArrayDrag[i].belongsToList=droppableIdcheck;
+                let startindex=0;
+                let endindex=0;
+                var d;
+
+                for(d in newCardArrayDrag){
+                    
+                    if(newCardArrayDrag[d].belongsToList===droppableIdStartcheck){
+                        
+                         break;
                     }
                 } 
+
+                console.log("DDD Index",d);
+
+                let a = parseInt(d);
+
+                console.log("Droppable Index Start",droppableIndexStart);
+                console.log("Droppable Index End",droppableIndexEnd);
+                startindex = a + droppableIndexStart;
+                endindex = a + droppableIndexEnd;
+
+                console.log("START and End Index",startindex,endindex);
+
+
+                let addCard = newCardArrayDrag.splice(startindex, 1);
+                newCardArrayDrag.splice(endindex, 0, addCard[0]);
+            }
+
+            if(droppableIdStartcheck !== droppableIdEndcheck){
+                var z;
+                for(z in newCardArrayDrag){
+                    
+                    if(newCardArrayDrag[z].cardId===cardIdcheck){
+                         break;
+                    }
+                } 
+
+                console.log("Index",z);
+                let addCard1 = newCardArrayDrag.splice(z, 1);
+                console.log(addCard1,newCardArrayDrag);
+
+                addCard1[0].belongsToList = droppableIdEndcheck;
+
+                let insertindex=0;
+                var p;
+
+                for(p in newCardArrayDrag){
+                    
+                    if(newCardArrayDrag[p].belongsToList===droppableIdEndcheck){
+                        
+                         break;
+                    }
+                } 
+
+                console.log("DDD Unequal  Index",p);
+
+                let m =parseInt(p);
+
+                insertindex=m + droppableIndexEnd;
+               
+                newCardArrayDrag.splice(insertindex,0,addCard1[0]);
+
+                //newCardArrayDrag.sort(compare);
 
 
             }
             console.log("New Array after drag",newCardArrayDrag);
 
-            return {...state,cardArray:newCardArrayDrag};
+            return { ...state,cardArray:newCardArrayDrag};
 
         case "DELETECARD":
             
@@ -128,6 +203,7 @@ const ListsReducer = (state= initialState, action) => {
                     newCardArrayMove[i].belongsToList=action.payload.destListid;
                 }
             } 
+            newCardArrayMove.sort(compare);
 
             return { ...state,cardArray:newCardArrayMove};
 
